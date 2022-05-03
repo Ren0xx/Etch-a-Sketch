e2d9fc87315bd5f93adac713940513f3e398cf7f
Adam Załęski <adamzaleski001@wp.pl>
2022-05-03 19:11:09 +0200

    Creating eventListener method for all divs

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const container = document.querySelector('.container');
 
+//creating 16 x 16 divs
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
-    div.textContent = '';
     div.setAttribute('class', 'grid--item');
     container.appendChild(div);
 }
+
+const divs = container.querySelectorAll('div');
+
+divs.forEach(div => {
+    div.addEventListener("mouseover", function( event ){
+        event.target.style.backgroundColor = 'black';
+    },)
+    
+});
