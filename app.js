7588c746b71be24f00e41ef2053a4bda099dacd6
Adam Załęski <adamzaleski001@wp.pl>
2022-05-05 22:00:09 +0200

    Creating funtions, user input and validation and generating random color for each square

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-const container = document.querySelector('.container');
-const buttons = document.querySelector('.buttons')
-
-
-function makeRows(per_side) {
-    container.style.setProperty('--grid-per_side', per_side);
-    for (c = 0; c < (per_side * per_side); c++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "grid--item";
-    };
+function makeGrid(per_side) {
+  container.style.setProperty("--grid-per_side", per_side);
+  for (c = 0; c < per_side * per_side; c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid--item";
   };
-makeRows(16); // staring point
+};
+function addEvents(divs) {
+  divs.forEach((div) => {
+    div.addEventListener("mouseover",  (event) => {
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      console.log(randomColor);
+      event.target.style.backgroundColor =  "#" + randomColor;
+    });
+  });
+};
 
-const divs = container.querySelectorAll('div');
+const container = document.querySelector(".container");
+const buttons = document.querySelector(".buttons");
 
-divs.forEach(div => {
-    div.addEventListener("mouseover", function( event ){
-        event.target.style.backgroundColor = 'black';
-    },)
-    
+const restartButton = document.createElement("button");
+restartButton.textContent = "Restart";
+buttons.appendChild(restartButton).className = "restart--button";
+
+
+makeGrid(16); // staring point
+
+const divs = container.querySelectorAll("div");
+
+addEvents(divs);
+
+restartButton.addEventListener("click", function (event) {
+  // great method to remember!
+  let result = window.prompt(
+    "How big should new grid be(in squares per side)?",
+    12
+  );
+  result = parseInt(result);
+  if (!Number.isInteger(result) || result <= 0 || result > 100) {
+    alert("Please enter a number within range (1, 100)");
+  } else {
+    container.replaceChildren();
+    makeGrid(result); // creating 
+    const divs = container.querySelectorAll("div");
+    addEvents(divs);
+  }
 });
-
-
-const button = document.createElement('button');
-button.textContent = 'Restart';
-buttons.appendChild(button).className = "restart--button";
-
-
-// button.addEventListener("click", function( event ){
-
-// })
-
-
-
